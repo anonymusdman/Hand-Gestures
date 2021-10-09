85e74288ebabc5758791f8d0ccf7d17c06498a1d
anonymusdman <77517905+anonymusdman@users.noreply.github.com>
2021-10-09 16:42:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var camera = document.getElementById("camera");
+
+Webcam.set({
+    width:350,
+    height:200,
+    image_format:"jpeg",
+    jpeg_quality:90
+});
+
+Webcam.attach('#camera');
+
+function take_photo(){
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = "<img id = 'captured_image' src = '" + data_uri + "'>"
+    })
+}
+
+console.log("ML5 Version: ", ml5.version);
+
+var classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/aw2fT1pLL/model.json", modelLoaded);
+
+function modelLoaded(){
+    console.log("Model Loaded");
+}
+
+function check_image(){
+    var img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("prediction").innerHTML = results[0].label;
+        document.getElementById("prediction_percentage").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
